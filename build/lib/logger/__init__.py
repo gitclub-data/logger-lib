36d20c5c8eaba2e530b51144dea6455f95bb3dd7
gaurav pandey <gauravpandey1207@gmail.com>
2026-02-08 15:06:53 +0530

    Remove build, dist, and egg-info from tracking

--- build/lib/logger/__init__.py
+++
@@ -1,9 +0,0 @@
-from .src.logger import Logger  # main logger class
-from .src.loggerDecorator import gaurav_logger  # decorator class
-from .src.loggerMessageDecorators import SimpleLogger, LoggerMessageDecorator, LoggerWithServiceName, LoggerWithTimeStamp  # Decorators to add details with logger
-from .src.logLevelEnum import LoglevelEnum  # log status
-from .src.writeLogMessage import WriteLogMessage, AsyncFileWriterLog, FileWriterLog, WriteLogsInQueue # write logs into certain filess
-
-__all__ = ['Logger', 'gaurav_logger', 'SimpleLogger', 'LoggerMessageDecorator', 'LoggerWithServiceName',
-           'LoggerWithTimeStamp', 'LoglevelEnum', 'WriteLogMessage', 'AsyncFileWriterLog',
-           'FileWriterLog', 'WriteLogsInQueue'] 
